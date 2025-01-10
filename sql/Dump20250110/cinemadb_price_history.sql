362e36cf0e82d2f655651f25c66b7b980ec0ca44
vlk34 <vlk34vlk34@gmail.com>
2025-01-10 21:06:28 +0300

    db

---
+++ sql/Dump20250110/cinemadb_price_history.sql
@@ -0,0 +1,53 @@
+-- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
+--
+-- Host: localhost    Database: cinemadb
+-- ------------------------------------------------------
+-- Server version	8.0.40
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `price_history`
+--
+
+DROP TABLE IF EXISTS `price_history`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `price_history` (
+  `change_date` date DEFAULT NULL,
+  `item` varchar(100) DEFAULT NULL,
+  `old_price` decimal(10,2) DEFAULT NULL,
+  `new_price` decimal(10,2) DEFAULT NULL,
+  `updated_by` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `price_history`
+--
+
+LOCK TABLES `price_history` WRITE;
+/*!40000 ALTER TABLE `price_history` DISABLE KEYS */;
+INSERT INTO `price_history` VALUES ('2025-01-08','Hall_A Ticket Price',12.00,25.00,'Volkan Erdogan'),('2025-01-08','Hall_A Ticket Price',25.00,31.00,'Volkan Erdogan'),('2025-01-08','Hall_A Ticket Price',31.00,12.00,'Volkan Erdogan'),('2025-01-08','Age Discount',50.00,31.00,'Volkan Erdogan'),('2025-01-08','Hall_B Ticket Price',50.00,31.00,'Volkan Erdogan'),('2025-01-08','Hall_B Ticket Price',31.00,50.00,'Volkan Erdogan'),('2025-01-08','Hall_A Ticket Price',12.00,25.00,'Volkan Erdogan'),('2025-01-08','Age Discount',31.00,50.00,'Volkan Erdogan'),('2025-01-09','Hall_A Ticket Price',25.00,31.00,'Ezgi Erdoğan'),('2025-01-09','Age Discount',50.00,46.00,'Ezgi Erdoğan'),('2025-01-09','Age Discount',46.00,50.00,'Ezgi Erdoğan'),('2025-01-09','Age Discount',50.00,22.00,'Ezgi Erdoğan'),('2025-01-09','Age Discount',22.00,50.00,'Ezgi Erdoğan'),('2025-01-10','Hall_A Ticket Price',31.00,30.00,'Volkan Erdogan');
+/*!40000 ALTER TABLE `price_history` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-01-10 21:04:12
